7c58fa8a1abc87d90f8eedaf2727a5ea400a819e
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-12 09:17:12 -0200

    add initial events

--- sources/scripts/mn-select.class.js
+++ sources/scripts/mn-select.class.js
@@ -1,7 +1,45 @@
 class MnSelect extends HTMLElement {
   constructor(self) {
     self = super(self)
+    this.setOpenEvents()
+    this.setCloseEvents()
     return self
+  }
+
+  setOpenEvents() {
+    const open = this.open
+    this.addEventListener('click', open)
+  }
+
+  setCloseEvents() {
+    const close = this.close
+    document.body.addEventListener('click', close)
+    document.addEventListener('keyup', () => {
+      const esc = event.keyCode === 27
+      let isOpened = document.body.classList.contains('mn-select-visible')
+
+      if (esc && isOpened) {
+        const select = document.querySelector('mn-select.visible')
+        document.body.classList.remove('mn-select-visible')
+        select.classList.remove('visible')
+      }
+    })
+  }
+
+  open() {
+    this.classList.add('visible')
+    document.body.classList.add('mn-select-visible')
+  }
+
+  close(event) {
+    event.stopPropagation()
+    const clickOutside = event.target.tagName === 'BODY'
+    const select = document.querySelector('mn-select.visible')
+
+    if (clickOutside && select) {
+      document.body.classList.remove('mn-select-visible')
+      select.classList.remove('visible')
+    }
   }
 }
 
