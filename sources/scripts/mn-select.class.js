397562152f1fb52d34547967c6ff9028cc2e74eb
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-21 14:09:33 -0200

    draft mobile options

--- sources/scripts/mn-select.class.js
+++ sources/scripts/mn-select.class.js
@@ -1,17 +1,18 @@
 class MnSelect extends HTMLElement {
   constructor(self) {
     self = super(self)
-    this.setOpenEvents()
-    this.setCloseEvents()
     this.setMenu()
+    this.setMobileOptions()
     this.setSelected()
     this.setOptionEvents()
+    this.setOpenEvents()
+    this.setCloseEvents()
     return self
   }
 
   setSelected() {
     const selected = document.createElement('div')
-    const selectedOption = this.querySelector('option[selected]') || this.querySelector('option')
+    const selectedOption = this.querySelector('.mn-select-option[selected]') || this.querySelector('.mn-select-option')
     selected.textContent = selectedOption.textContent
     this.insertBefore(selected, this.firstChild)
   }
@@ -24,9 +25,37 @@ class MnSelect extends HTMLElement {
 
     Array
       .from(this.children)
-      .forEach(child => menu.appendChild(child))
+      .forEach(child => {
+        const fallbackOption = document.createElement('div')
+        fallbackOption.classList.add('mn-select-option')
+        fallbackOption.textContent = child.textContent
+
+        Array
+          .from(child.attributes)
+          .forEach(attr => fallbackOption.setAttribute(attr.name, attr.value))
+
+        child.parentNode.removeChild(child)
+        menu.appendChild(fallbackOption)
+      })
 
     this.insertBefore(menu, this.firstChild)
+  }
+
+  setMobileOptions() {
+    const options = document.createElement('div')
+    const menu = this.querySelector('menu').cloneNode(true)
+    const cancelButton = document.createElement('button')
+
+    options.classList.add('mn-mobile-options')
+    menu.classList.remove('mn-card')
+    menu.removeAttribute('style')
+    cancelButton.textContent = 'cancel'
+
+    this.mobileOptions = options
+    options.append(menu)
+    options.append(cancelButton)
+
+    document.body.append(options)
   }
 
   setOpenEvents() {
@@ -35,18 +64,21 @@ class MnSelect extends HTMLElement {
   }
 
   setOptionEvents() {
-    const options = this.querySelectorAll('option')
+    const options = this.querySelectorAll('.mn-select-option')
+    const mobileOptions = Array.from(this.mobileOptions.querySelectorAll('.mn-select-option'))
 
     Array
       .from(options)
+      .concat(mobileOptions)
       .forEach(option => option.addEventListener('click', event => {
         this.setSelectedOption(event.target)
-        this.setValue(event.target.value)
+        this.setValue(event.target.getAttribute('value'))
         this.close()
       }))
   }
 
   setCloseEvents() {
+    this.mobileOptions.querySelector('button').addEventListener('click', () => this.close())
     document.body.addEventListener('click', this.close)
     document.addEventListener('keyup', () => {
       const esc = event.keyCode === 27
@@ -59,7 +91,7 @@ class MnSelect extends HTMLElement {
   }
 
   setValue(value) {
-    const option = this.querySelector(`option[value="${value}"]`)
+    const option = this.querySelector(`.mn-select-option[value="${value}"]`)
     const viewValue = option
       ? option.textContent
       : null
@@ -80,7 +112,7 @@ class MnSelect extends HTMLElement {
   setSelectedOption(target) {
     // const value = target.value || target.textContent
     // target.parentNode.setAttribute('value', value)
-    const lastSelected = target.parentNode.querySelector('option[selected]')
+    const lastSelected = target.parentNode.querySelector('.mn-select-option[selected]')
 
     if (lastSelected) {
       lastSelected.removeAttribute('selected')
@@ -89,10 +121,11 @@ class MnSelect extends HTMLElement {
   }
 
   open(event) {
-    if (event && event.target.tagName === 'OPTION') {
+    if (event && event.target.classList.contains('.mn-select-option')) {
       return false
     }
     this.classList.add('visible')
+    this.mobileOptions.classList.add('visible')
     document.body.classList.add('mn-select-visible')
   }
 
@@ -101,16 +134,21 @@ class MnSelect extends HTMLElement {
     if (event && select) {
       event.stopPropagation()
       const clickOutside = event.target.tagName === 'BODY'
+        || event.target.classList.contains('mn-mobile-options')
+        || event.target.classList.contains('mn-select-option')
 
       if (clickOutside && select) {
         document.body.classList.remove('mn-select-visible')
         select.classList.remove('visible')
+        select.mobileOptions.classList.remove('visible')
       }
     } else if (select) {
       document.body.classList.remove('mn-select-visible')
       select.classList.remove('visible')
+      select.mobileOptions.classList.remove('visible')
     }
   }
 }
 
 customElements.define('mn-select', MnSelect)
+
