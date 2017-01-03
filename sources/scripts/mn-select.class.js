a6df5b2fcd103d75ad6279b3bcf5f1d3d9555116
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-02 22:52:49 -0200

    begin refactor

--- sources/scripts/mn-select.class.js
+++ sources/scripts/mn-select.class.js
@@ -1,77 +1,20 @@
 class MnSelect extends HTMLElement {
   constructor(self) {
     self = super(self)
-    this.setTabIndex()
+    this.tabIndex()
     this.setMenu()
-    this.setMobileOptions()
+    this.setMobile()
     this.setSelected()
     this.setOptionEvents()
     this.setOpenEvents()
-    // this.setArrowEvents()
     this.setCloseEvents()
     this.setNameGetter()
-    // this.setRequired()
     return self
   }
 
-  // setRequired() {
-  //   const isRequired = this.getAttribute('required')
-  //   const value = this.value
-  //   console.log(this.validity)
-  //   if (isRequired && !value) {
-  //     this.willValidate = true
-  //     console.log(this.willValidate)
-  //     this.setCustomValidity('is required')
-  //   }
-  // }
-
-  setNameGetter() {
-    const form = this.closest('form')
-    const name = this.getAttribute('name')
-    const element = this
-
-    if (form && name) {
-      Object.defineProperty(form, name, {get: () => element})
-    }
-  }
-
-  setTabIndex() {
+  tabIndex() {
     const tabindex = this.getAttribute('tabindex') || '0'
     this.setAttribute('tabindex', tabindex)
-    const options = this.querySelectorAll('option')
-
-    Array
-      .from(options)
-      .forEach(option => option.setAttribute('tabindex', '0'))
-  }
-
-  setSelected() {
-    const selected = document.createElement('div')
-    const selectedOption = this.getAttribute('placeholder')
-      ? this.querySelector('.mn-select-option[selected]')
-      : this.querySelector('.mn-select-option[selected]') || this.querySelector('.mn-select-option')
-
-    if (selectedOption) {
-      const value = selectedOption.getAttribute('value') || selectedOption.textContent
-      this.value = value
-      this.classList.add('has-value')
-      selected.textContent = selectedOption.textContent
-    }
-    this.insertBefore(selected, this.firstChild)
-  }
-
-  set value(value) {
-    this.setValue(value)
-    this.setAttribute('value', value)
-    const target = this.querySelector(`.mn-select-option[value="${value}"]`)
-
-    if (target) {
-      this.setSelectedOption(target)
-    }
-  }
-
-  get value() {
-    return this.getAttribute('value') || undefined
   }
 
   setMenu() {
@@ -98,7 +41,7 @@ class MnSelect extends HTMLElement {
     this.insertBefore(menu, this.firstChild)
   }
 
-  setMobileOptions() {
+  setMobile() {
     const options = document.createElement('div')
     const menu = this.querySelector('menu').cloneNode(true)
     const cancelButton = document.createElement('button')
@@ -115,37 +58,20 @@ class MnSelect extends HTMLElement {
     document.body.append(options)
   }
 
-  setOpenEvents() {
-    const open = this.open
-    this.addEventListener('click', open)
-    this.addEventListener('keyup', event => {
-      if (event.key === 'Enter' || event.key === ' ') {
-        this.open(event)
-      }
-    })
+  setSelected() {
+    const selected = document.createElement('div')
+    const selectedOption = this.getAttribute('placeholder')
+      ? this.querySelector('.mn-select-option[selected]')
+      : this.querySelector('.mn-select-option[selected]') || this.querySelector('.mn-select-option')
+
+    if (selectedOption) {
+      const value = selectedOption.getAttribute('value') || selectedOption.textContent
+      this.value = value
+      this.classList.add('has-value')
+      selected.textContent = selectedOption.textContent
+    }
+    this.insertBefore(selected, this.firstChild)
   }
-
-  // setArrowEvents() {
-  //   this.addEventListener('keyup', event => {
-  //     const keyIsArrowUpOrDown = event.key === 'ArrowUp' || event.key === 'ArrowDown'
-
-  //     if (keyIsArrowUpOrDown) {
-  //       this.open(event)
-  //     }
-  //   })
-
-  //   const options = this.querySelectorAll('.mn-select-option')
-
-  //   Array
-  //     .from(options)
-  //     .forEach(option => option.addEventListener('keyup', event => {
-  //       if (event.key === 'ArrowDown') {
-  //         event.target.previousElementSibling.focus()
-  //       } else if (event.key === 'ArrowDown') {
-  //         event.target.nextElementSibling.focus()
-  //       }
-  //     }))
-  // }
 
   setOptionEvents() {
     const options = this.querySelectorAll('.mn-select-option')
@@ -160,6 +86,16 @@ class MnSelect extends HTMLElement {
         this.value = value
         this.close()
       }))
+  }
+
+  setOpenEvents() {
+    const open = this.open
+    this.addEventListener('click', open)
+    this.addEventListener('keyup', event => {
+      if (event.key === 'Enter' || event.key === ' ') {
+        this.open(event)
+      }
+    })
   }
 
   setCloseEvents() {
@@ -193,6 +129,30 @@ class MnSelect extends HTMLElement {
     })
   }
 
+  setNameGetter() {
+    const form = this.closest('form')
+    const name = this.getAttribute('name')
+    const element = this
+
+    if (form && name) {
+      Object.defineProperty(form, name, {get: () => element})
+    }
+  }
+
+  set value(value) {
+    this.setValue(value)
+    this.setAttribute('value', value)
+    const target = this.querySelector(`.mn-select-option[value="${value}"]`)
+
+    if (target) {
+      this.setSelectedOption(target)
+    }
+  }
+
+  get value() {
+    return this.getAttribute('value') || undefined
+  }
+
   setValue(value) {
     const option = this.querySelector(`.mn-select-option[value="${value}"]`)
     const viewValue = option
@@ -214,8 +174,6 @@ class MnSelect extends HTMLElement {
   }
 
   setSelectedOption(target) {
-    // const value = target.value || target.textContent
-    // target.parentNode.setAttribute('value', value)
     const lastSelected = target.parentNode.querySelector('.mn-select-option[selected]')
 
     if (lastSelected) {
