db33f6bf36a6ca73f05c727d82d373ba734e603d
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-03 00:46:11 -0200

    fix select option in mobile

--- sources/scripts/mn-select.class.js
+++ sources/scripts/mn-select.class.js
@@ -83,6 +83,7 @@ class MnSelect extends HTMLElement {
       .from(options)
       .concat(mobile)
       .forEach(option => option.addEventListener('click', event => {
+        console.log('click')
         const value = event.target.getAttribute('value') || event.target.textContent
         this.value = value
         this.close()
@@ -102,7 +103,7 @@ class MnSelect extends HTMLElement {
   }
 
   setCloseEvents() {
-    this.addEventListener('blur', () => this.close())
+    this.addEventListener('focus', () => this.close())
     this.mobile.querySelector('button').addEventListener('click', () => this.close())
     // need check if click in mobile outside works with event below to document click
     // this.mobile.addEventListener('touchend', event => {
@@ -124,11 +125,10 @@ class MnSelect extends HTMLElement {
       }
     })
     document.addEventListener('click', event => {
-      // const clickOutside = !event.target.closest('mn-select')
+      const clickOutside = !event.target.closest('mn-select')
       const selectOption = event.target.classList.contains('mn-select-option')
 
-      // if (clickOutside || selectOption) {
-      if (selectOption) {
+      if (clickOutside || selectOption) {
         this.close()
       }
     })
